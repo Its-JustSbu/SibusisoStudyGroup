2b6c47d27f672cecebdbc20108b2d3b89561eb3d
Sibusiso Makhoba <116superstm@gmail.com>
2023-03-08 13:54:38 +0200

    Model Students Created.

---
+++ SibusisoStudyGroup/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SibusisoStudyGroup.Models
+{
+    public class Student
+    {
+        public string studentNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string emailAddress { get; set; }
+    }
+}
