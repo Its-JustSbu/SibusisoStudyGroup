9e5163978ab873652aee3fdff5e519632af1817e
Sibusiso Makhoba <116superstm@gmail.com>
2023-03-10 08:25:54 +0200

    Updated List and Navbar

--- SibusisoStudyGroup/Controllers/StudentController.cs
+++ SibusisoStudyGroup/Controllers/StudentController.cs
@@ -15,7 +15,10 @@ namespace SibusisoStudyGroup.Controllers
             List<Models.Student> students = new List<Models.Student>();
 
             students.Add(new Models.Student { studentNumber = "u22699032", Name = "Sibusiso", Surname = "Makhoba", emailAddress = "u22699032@tuks.co.za"});
-            students.Add(new Models.Student { studentNumber = "", Name = "", Surname = "", emailAddress = "" });
+            students.Add(new Models.Student { studentNumber = "u11111111", Name = "Graham", Surname = "Potter", emailAddress = "GP@chelsea.com" });
+            students.Add(new Models.Student { studentNumber = "u22222222", Name = "Mason", Surname = "Mount", emailAddress = "MM@chelsea.com" });
+            students.Add(new Models.Student { studentNumber = "u33333333", Name = "Reece", Surname = "James", emailAddress = "RJ@chelsea.com" });
+            students.Add(new Models.Student { studentNumber = "u44444444", Name = "Thiago", Surname = "Silva", emailAddress = "TS@chelsea.com" });
 
             return View(students);
         }
